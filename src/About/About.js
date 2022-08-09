96654bd888bba7686ae283b7928f9b52b13d8c31
Basil <96963015+basilspice@users.noreply.github.com>
2022-08-09 13:48:29 -0700

    Update About.js

--- src/About/About.js
+++ src/About/About.js
@@ -3,7 +3,7 @@ export default function About () {
     return(
     <div className="footer">
     <footer>
-      code <span className='and'>by</span><br></br><a href="https://basilspice.github.io/Portfolio-1/" target="_blank"><button>Basil</button></a>
+      code <span className='and'>by</span><br></br><a href="https://basilspice.github.io/Portfolio/" target="_blank"><button>Basil</button></a>
     </footer>
     </div>
     )}
